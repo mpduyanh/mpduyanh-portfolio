56dc1a64741fa8aeb3069567b230c6101d59f8f0
mpduyanh <mpduyanh@gmail.com>
2023-08-15 15:25:16 +0700

    Fix animations not working

--- src/main.js
+++ src/main.js
@@ -11,5 +11,19 @@ import '../styles/utils.css';
 
 import mobileNav from './utils/mobile-nav';
 
+
 mobileNav();
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddentElements = document.querySelectorAll('.hidden');
+hiddentElements.forEach((el) => observer.observe(el));
